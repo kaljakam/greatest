8d4a70b7c3749122344276c88332455abf78607a
Nick <kaljakam@ya.ru>
2017-08-31 16:11:29 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=greatest',
-    'username' => 'root',
-    'password' => 'mysql',
-    'charset' => 'utf8',
-];
